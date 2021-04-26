b2817a0fcdbdb13a8607f62d0f14b22167d08b96
Robert <Robert@song.com>
2021-04-26 19:16:35 +0800

    first commit

---
+++ src/main/java/com/song/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.song.git;
+
+/**
+ * @author xsonga
+ * @create 2021-04-26-19:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
