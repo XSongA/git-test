7152dc45d32125dba963de6ec55f1a522dcd2683
Robert <Robert@song.com>
2021-04-26 19:20:17 +0800

    second commit

--- src/main/java/com/song/git/GitTest.java
+++ src/main/java/com/song/git/GitTest.java
@@ -1,11 +1,8 @@
 package com.song.git;
 
-/**
- * @author xsonga
- * @create 2021-04-26-19:12
- */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello World!");
     }
 }
